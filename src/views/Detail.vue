98af37c768ae4032f003baae3afec43e9848f06d
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-01-11 22:50:20 +0800

    Create back and like icon

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,5 +1,13 @@
 <script setup>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
   import Caraousel from '@/components/Caraousel.vue'
+  
+  //Instance
+  const router = useRouter()
+  
+  //Heart handler
+  const isHeartClicked = ref(false)
   
   //Images
   const images = [
@@ -10,5 +18,14 @@
 </script>
 
 <template>
+  <!-- Navigation and icons -->
+  <section class="flex w-full justify-between">
+    <i @click="router.go(-1)" class="fa fa-arrow-left"></i>
+    <i 
+      @click="isHeartClicked = !isHeartClicked"
+      :class="isHeartClicked ? 'fas' : 'far'" 
+      class="fa-heart"></i>
+  </section>
+  
   <Caraousel :images="images" />
 </template>
