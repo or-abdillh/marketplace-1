a5c3e6901ff31f595730505f6c0df5180d0e473f
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-01-12 22:52:59 +0800

    Create animation class Card and Slide

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -15,7 +15,7 @@
 <template>
    <section
       :class="active ? 'bg-gray-800 text-gray-50 font-medium' : 'bg-white'"
-      class="px-3 py-1 transition-300 border border-gray-400 rounded-xl">
+      class="slide-animation px-3 py-1 transition-300 border border-gray-400 rounded-xl">
       <p class="text-xs">{{ label }}</p>
    </section>
 </template>
--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-   <section class="w-5.5/12" @click="router.push({ name: 'detail' })">
+   <section class="card-animation w-5.5/12" @click="router.push({ name: 'detail' })">
       <img src="shoes.png" class="p-2 bg-gray-50 rounded-xl" />
       <small class="text-xxs font-medium">New K Tuo</small>
       <h1 class="text-xs font-semibold">$99.10</h1>
